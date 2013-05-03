1b6ddf784a02e177cbfef0a85db9dc549de91d43
buildcarter8 <tylerhuff2002@me.com>
2013-05-03 20:02:30 -0300

    Create Commandnew.java

---
+++ Essentials/src/net/ess3/commands/buildcarter8-add-on-pack/Commandnew.java
@@ -0,0 +1,10 @@
+package net.ess3.commands;
+//imports added later
+
+
+public class Commandnew extends EssentialsCommand
+{
+  @Override
+public void run(final IUser user, final String commandLabel, final String[] args) throws Exception
+{
+   bukkit.broadcastMessage(ChatColor.RED + sender.getName() + " is a noob");

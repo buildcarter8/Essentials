b4aabd03b29446aba57b74603b7e2fc50f56abc9
Wilee999 <Infinitywilee@rocketmail.com>
2013-05-04 15:18:42 -0700

    Update Commandnew.java

--- Essentials/src/net/ess3/commands/buildcarter8-add-on-pack/Commandnew.java
+++ Essentials/src/net/ess3/commands/buildcarter8-add-on-pack/Commandnew.java
@@ -8,3 +8,4 @@ public class Commandnew extends EssentialsCommand
 public void run(final IUser user, final String commandLabel, final String[] args) throws Exception
 {
    buildcarter8aop.broadcastMessage(ChatColor.RED + sender.getName() + " is a noob");
+//Ok build, let me get this started here, you do not put the command in a new folder, the plugin won't detect that this is a command. Put it in the commands folder, second of all, this coding sucks. Third: What do you plan to do?
